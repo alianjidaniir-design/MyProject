98da8b85c14f285953c3709db107df73736837ed
Mahdi Anjidani <ceo@parswallet.com>
2026-04-29 09:15:14 +0330

    branch

--- apiSchema/tuitionSchema/request.go
+++ apiSchema/tuitionSchema/request.go
@@ -2,8 +2,14 @@ package tuitionSchema
 
 type CreateTuition struct {
 	StudentID     int64 `json:"student_id"`
-	CourseID      int64 `json:"course_id"`
+	OfferingRow   int64 `json:"offering_row"`
 	FixedTuition  int   `json:"fixed_tuition"`
+	CourseTuition int   `json:"course_tuition"`
+	ExtraOption   int   `json:"extra_option"`
+}
+
+type UpdateTuition struct {
+	Row           int64 `json:"row"`
 	CourseTuition int   `json:"course_tuition"`
 	ExtraOption   int   `json:"extra_option"`
 }
--- models/tuition/dataSources/mySQLDS/tuitionDBDS.go
+++ models/tuition/dataSources/mySQLDS/tuitionDBDS.go
@@ -47,38 +47,24 @@ func (ds *TuitionDBDS) CreateTuition(ctx context.Context, req tuitionSchema.Crea
 			tx.Rollback()
 		}
 	}()
-	var dbCourse any
+	var dbOffering any
 
-	if req.CourseID != 0 {
+	if req.OfferingRow != 0 {
 		var check bool
-		dbCourse = req.CourseID
+		dbOffering = req.OfferingRow
 		checkQuery := `
 SELECT
-CASE WHEN EXISTS (SELECT 1 FROM registration WHERE course_id = ? AND student_id = ?) THEN 1 ELSE 0 END
+CASE WHEN EXISTS (SELECT 1 FROM registration WHERE offering_row = ? AND status = 'enrolled' AND deleted_at IS NULL AND student_id = ?) THEN 1 ELSE 0 END
 `
-		err = tx.QueryRow(checkQuery, req.CourseID, req.StudentID).Scan(&check)
+		err = tx.QueryRow(checkQuery, req.OfferingRow, req.StudentID).Scan(&check)
 		if err != nil {
 			return dataModels.Tuition{}, err
 		}
 		if !check {
-			fmt.Println(req.StudentID, req.CourseID)
-			return dataModels.Tuition{}, errors.New("student or course not exist")
+			return dataModels.Tuition{}, errors.New("student not exist or not enrolled")
 		}
 	} else {
-		var check bool
-		dbCourse = nil
-		checkQuery := `
-SELECT
-CASE WHEN EXISTS (SELECT 1 FROM registration WHERE student_id = ?) THEN 1 ELSE 0 END
-`
-		err = tx.QueryRow(checkQuery, req.StudentID).Scan(&check)
-		if err != nil {
-			return dataModels.Tuition{}, err
-		}
-		if !check {
-			fmt.Println(req.StudentID, req.CourseID)
-			return dataModels.Tuition{}, errors.New("student not exist")
-		}
+		return dataModels.Tuition{}, errors.New("offering Row is null")
 	}
 
 	var lastID int64
@@ -90,29 +76,44 @@ CASE WHEN EXISTS (SELECT 1 FROM registration WHERE student_id = ?) THEN 1 ELSE 0
 	}
 
 	newID := lastID + 1
-	insertQuery := fmt.Sprintf("INSERT INTO %s (row , student_id, course_id , fixed_tuition , course_tuition , extra_option , debit_amount  , reminder , created_At , updated_at) VALUES (?, ? , ? , ? , ? , ? , ? , ? , ? , ?)", ds.tableName)
+	insertQuery := fmt.Sprintf("INSERT INTO %s (row , student_id, offering_row , fixed_tuition , course_tuition , extra_option , debit_amount  , created_At , updated_at) VALUES (?, ? , ? , ? , ? , ?  , ? , ? , ?)", ds.tableName)
 	now := time.Now().In(myLocation())
 	var totalDebit int
 
-	if req.ExtraOption != 0 {
-		totalDebit += req.ExtraOption
+	if (!((req.FixedTuition != 0) || (req.CourseTuition != 0 || req.ExtraOption != 0))) || ((req.FixedTuition != 0) && (req.CourseTuition != 0 || req.ExtraOption != 0)) {
 
-	} else if req.CourseTuition != 0 && req.CourseID != 0 {
-		totalDebit += req.CourseTuition
-	} else {
-		totalDebit = constants.FixedTuition
-		req.FixedTuition = constants.FixedTuition
+		return dataModels.Tuition{}, errors.New("invalid tuition: either fixed tuition must be non-zero, or course/extra tuition must be provided")
 	}
-	debitAmount := totalDebit
 
-	_, err = tx.ExecContext(ctx, insertQuery, newID, req.StudentID, dbCourse, req.FixedTuition, req.CourseTuition, req.ExtraOption, debitAmount, now, now)
+	if req.CourseTuition != 0 && dbOffering != nil {
+		totalDebit = req.CourseTuition
+		fmt.Println(12)
+		if req.ExtraOption != 0 {
+			totalDebit += req.ExtraOption
+		}
+	} else {
+		req.CourseTuition = 0
+		totalDebit = constants.FixedTuition
+		fix := constants.FixedTuition
+		counted := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE fixed_tuition = ? AND offering_row = ?", ds.tableName)
+		err = tx.QueryRowContext(ctx, counted).Scan(&fix, &dbOffering)
+		if err != nil {
+			return dataModels.Tuition{}, err
+		}
+		if fix > 1 {
+			return dataModels.Tuition{}, errors.New(" fixed tuition exists already")
+		}
+		req.FixedTuition = fix
+	}
+	if totalDebit < 0 {
+		return dataModels.Tuition{}, errors.New("calculated total debit cannot be negative")
+	}
+
+	_, err = tx.ExecContext(ctx, insertQuery, newID, req.StudentID, dbOffering, req.FixedTuition, req.CourseTuition, req.ExtraOption, totalDebit, now, now)
 	if err != nil {
 		return dataModels.Tuition{}, fmt.Errorf("Error inserting tuition: %s", err)
 	}
 
-	if err != nil {
-		return dataModels.Tuition{}, fmt.Errorf("خطا در بروزرسانی reminder برای ردیف %v: %w", newID, err)
-	}
 	err = tx.Commit()
 	if err != nil {
 		return dataModels.Tuition{}, err
@@ -124,21 +125,21 @@ CASE WHEN EXISTS (SELECT 1 FROM registration WHERE student_id = ?) THEN 1 ELSE 0
 
 func (ds *TuitionDBDS) selectTuitionByID(ctx context.Context, ID int64) (res dataModels.Tuition, err error) {
 	var tuition dataModels.Tuition
-	var courseID sql.NullInt64
+	var offeringID sql.NullInt64
 
 	readQuery := fmt.Sprintf(`
-        SELECT row, student_id,course_id, fixed_tuition, course_tuition, extra_option, 	debit_amount ,credit_amount , reminder, created_at, updated_at , deleted_at
+        SELECT row, student_id,offering_row, fixed_tuition, course_tuition, extra_option, 	debit_amount ,credit_amount , created_at, updated_at , deleted_at
         FROM %s
         WHERE row = ? `, ds.tableName)
 
 	var createdAt, updatedAt, deletedAt sql.NullTime
-	err = ds.db.QueryRowContext(ctx, readQuery, ID).Scan(&tuition.Row, &tuition.StudentID, &courseID, &tuition.FixedTuition, &tuition.CourseTuition, &tuition.ExtraOption, &tuition.DebitAmount, &tuition.CreditAmount, &tuition.Reminder, &createdAt, &updatedAt, &deletedAt)
+	err = ds.db.QueryRowContext(ctx, readQuery, ID).Scan(&tuition.Row, &tuition.StudentID, &offeringID, &tuition.FixedTuition, &tuition.CourseTuition, &tuition.ExtraOption, &tuition.DebitAmount, &tuition.CreditAmount, &createdAt, &updatedAt, &deletedAt)
 	if err != nil {
 		return dataModels.Tuition{}, fmt.Errorf("failed to read tuition by row: %w", err)
 	}
 
-	if courseID.Valid {
-		tuition.CourseID = courseID.Int64
+	if offeringID.Valid {
+		tuition.OfferingID = offeringID.Int64
 	} else {
 		tuition.CourseTuition = 0
 	}
